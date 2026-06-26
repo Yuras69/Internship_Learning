08dbb2828d7fdafcd2b0e5e4b75ec6ca87ec77db
Yuras69 <uraspokhrel25@gmail.com>
2026-06-26 13:32:36 +0545

    variable decleration

---
+++ JavaScriptLearning/01_Basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "uraspokharel@gmail.com"
+var accountPassword = "12345"
+let accountCity = "Biratnagar"
+
+accountEmail = "hc@.com"
+accountPassword = "0101"
+accountCity = "India"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
