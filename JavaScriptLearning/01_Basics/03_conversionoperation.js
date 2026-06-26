6faf3b4c9f0e63105c1951e49ce4ee766421bf0e
Yuras69 <uraspokhrel25@gmail.com>
2026-06-26 23:25:09 +0545

    learning conversion

--- JavaScriptLearning/01_Basics/03_conversionoperation.js
+++ JavaScriptLearning/01_Basics/03_conversionoperation.js
@@ -19,5 +19,24 @@
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//********************operations******************/
+
+let value = 3
+let negValue = -(value)
+//console.log(negValue);
+
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+//consile.log(3%2);
+
+
+let str1 = "hello"
+let str2 = " Yuras"
+
+let str3 = str1 + str2 
+console.log(str3);
