99c8b772b5d3baf3798f01ff5c59c24ad5428097
Yuras69 <uraspokhrel25@gmail.com>
2026-06-26 13:33:43 +0545

    Learning Datatypes

---
+++ JavaScriptLearning/01_Basics/02_DataType.js
@@ -0,0 +1,15 @@
+"use Strict";
+
+let name = "Yuras"
+let isLoggedIn = false
+let state;
+
+console.log(typeof  "Yuras");
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
