290334f7ac18aba621ef944f7a34b55960c03e9f
Yuras69 <uraspokhrel25@gmail.com>
2026-06-26 13:54:52 +0545

    ConversionOfDataType

---
+++ JavaScriptLearning/01_Basics/03_conversionoperation.js
@@ -0,0 +1,23 @@
+//let score = "33" 
+
+//console.log(typeof score);
+
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+//let isloggedIn = ""
+
+//let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+// "" => false
+// "yuras" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
